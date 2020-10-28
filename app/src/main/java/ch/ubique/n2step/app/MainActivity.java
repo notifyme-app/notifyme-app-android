7ce061048b31c5f7362ca5a3d7ff7a32e7bd42aa
Johannes Gallmann <gallmann@ubique.ch>
2020-10-28 19:01:56 +0100

    Create empty project

---
+++ app/src/main/java/ch/ubique/n2step/app/MainActivity.java
@@ -0,0 +1,21 @@
+package ch.ubique.n2step.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ch.ubique.n2step.app.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_activity);
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.replace(R.id.container, MainFragment.newInstance())
+					.commitNow();
+		}
+	}
+
+}
---
+++ app/src/main/java/ch/ubique/n2step/app/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package ch.ubique.n2step.app.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+	// TODO: Implement the ViewModel
+}
