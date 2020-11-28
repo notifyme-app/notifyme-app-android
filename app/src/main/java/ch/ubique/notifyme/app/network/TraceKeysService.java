ba2655a95bb3095fa820f8189600f58f007b2a0b
Johannes Gallmann <gallmann@ubique.ch>
2020-11-28 10:47:23 +0100

    Update SDK

--- app/src/main/java/ch/ubique/notifyme/app/network/TraceKeysService.java
+++ app/src/main/java/ch/ubique/notifyme/app/network/TraceKeysService.java
@@ -8,7 +8,7 @@ import retrofit2.http.Query;
 
 public interface TraceKeysService {
 
-	@Headers("Accept: application/protobuf")
+	@Headers("Accept: application/x-protobuf")
 	@GET("v1/traceKeys")
 	Call<ResponseBody> getTraceKeys(@Query("lastKeyBundleTag") long lastKeyBundleTag);
 
